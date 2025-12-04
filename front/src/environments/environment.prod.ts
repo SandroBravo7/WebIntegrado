db33ddd58a77fb665d85574642e08e8cd785c1ff
Sandro Bravo <U22308035@utp.edu.pe>
2025-12-03 21:49:16 -0500

    Configuración final de servicios y environment para despliegue

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  // Aquí usamos la URL de Render (con HTTPS)
+  urlHost: 'https://webintegrado.onrender.com/',
+  urlApi: 'https://webintegrado.onrender.com/api/v1/',
+  urlApi2: 'https://webintegrado.onrender.com/auth/'
 };
